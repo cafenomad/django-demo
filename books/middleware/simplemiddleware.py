1d35328c0082e54209b5a790c33de05fba7b9137
cafenomad <cafenomad@outlook.com>
2019-04-23 12:20:50 +0900

    Change SimpleMiddleware

--- books/middleware/simplemiddleware.py
+++ books/middleware/simplemiddleware.py
@@ -2,15 +2,16 @@ class SimpleMiddleware:
 
     def __init__(self, get_response):
         self.get_response = get_response
-
-        print("[Middleware] init")
+        print("[SimpleMiddleware] init")
 
     def __call__(self, request):
-
-        print("[Middleware] before the view")
-
-        response = self.get_response(request)
-
-        print("[Middleware] after the view")
-
+        self.process_request(request)  # Before process
+        response = self.get_response(request)  # View process
+        self.process_response(request, response)  # After process
         return response
+
+    def process_request(self, request):
+        print("[SimpleMiddleware] process request")
+
+    def process_response(self, request, response):
+        print("[SimpleMiddleware] process response")
